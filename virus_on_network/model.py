24f015978112a66f6970e5248f7e659a97497687
Kyle Chandrasena <kchandrasena10@gmail.com>
2025-03-11 17:48:32 -0400

    Implemented reproduction rates and UI changes

--- virus_on_network/model.py
+++ virus_on_network/model.py
@@ -29,6 +29,21 @@ def number_fact_checkers(model):
     return number_state(model, State.FACT_CHECKER)
 
 
+def number_userInfected(model):
+    return model.userInfected
+
+
+def number_botInfected(model):
+    return model.botInfected
+
+def reproduction_userInfected(model):
+    return model.userInfected / number_misinformed(model)
+
+def reproduction_botInfected(model):
+    return model.botInfected / number_misinformed(model)
+
+
+
 class VirusOnNetwork(Model):
     """A misinformation spread model with all agent types including misinformation bots."""
 
@@ -40,8 +55,9 @@ class VirusOnNetwork(Model):
         initial_outbreak_size=1,
         virus_spread_chance=0.4,
         virus_check_frequency=0.4,
-        recovery_chance=0.3,
-        gain_resistance_chance=0.5,
+        # recovery_chance=0.3,
+        # gain_resistance_chance=0.5,
+        resistance_duration=6,
         fact_checker_ratio=0.1,
         misinformation_bot_ratio=0.1,
         seed=None,
@@ -49,6 +65,8 @@ class VirusOnNetwork(Model):
         super().__init__(seed=seed)
         self.num_nodes = num_nodes
         prob = avg_node_degree / self.num_nodes
+        self.userInfected = 0
+        self.botInfected = 0
         self.G = nx.erdos_renyi_graph(n=self.num_nodes, p=prob)
         while not nx.is_connected(self.G):
             self.G = nx.erdos_renyi_graph(n=self.num_nodes, p=prob)
@@ -63,6 +81,8 @@ class VirusOnNetwork(Model):
                 "Susceptible": number_susceptible,
                 "Resistant": number_resistant,
                 "Fact Checkers": number_fact_checkers,
+                "User Misinformation Reproduction Rate": reproduction_userInfected,
+                "Bot Misinformation Reproduction Rate": reproduction_botInfected,
             }
         )
 
@@ -81,8 +101,9 @@ class VirusOnNetwork(Model):
                 state,
                 virus_spread_chance,
                 virus_check_frequency,
-                recovery_chance,
-                gain_resistance_chance,
+                # recovery_chance,
+                # gain_resistance_chance,
+                resistance_duration,
             )
             self.grid.place_agent(a, node)
 
@@ -94,8 +115,16 @@ class VirusOnNetwork(Model):
 
         self.running = True
         self.datacollector.collect(self)
+        global stepCount
+        stepCount = 0
 
 
     def step(self):
+        global stepCount
         self.agents.shuffle_do("step")
         self.datacollector.collect(self)
+        # Debugging statements to verify data collection
+        print(f"Step {stepCount}: User Infected Rate = {reproduction_userInfected(self):.2%}, Bot Infected Rate = {reproduction_botInfected(self):.2%}")
+        self.userInfected = 0
+        self.botInfected = 0
+        stepCount += 1
