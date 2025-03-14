e00391bc0e56db82eba6485db9808116458752fd
YuriM21 <70816113+YuriM21@users.noreply.github.com>
2025-03-14 18:42:20 -0400

    Updated agents.py for misinformed 1-3

--- virus_on_network/agents.py
+++ virus_on_network/agents.py
@@ -3,118 +3,79 @@ from mesa import Agent
 
 class State(Enum):
     SUSCEPTIBLE = 0            # Users not yet exposed to misinformation
-    MISINFORMED_USER = 1        # Users who believe and spread misinformation
-    RESISTANT = 2               # Users resistant to misinformation
-    FACT_CHECKER = 3            # Users who actively fact-check misinformation
-    MISINFORMATION_BOT = 4      # First strain of misinformation bots
-    MISINFORMATION_BOT_2 = 5    # Second strain of misinformation bots
-    MISINFORMATION_BOT_3 = 6    # Third strain of misinformation bots
+    MISINFORMED_USER = 1       # Users who believe and spread misinformation
+    RESISTANT = 2              # Users resistant to misinformation
+    FACT_CHECKER = 3           # Users who actively fact-check misinformation
+    MISINFORMATION_BOT = 4     # First strain of misinformation bots
+    MISINFORMATION_BOT_2 = 5   # Second strain of misinformation bots
+    MISINFORMATION_BOT_3 = 6   # Third strain of misinformation bots
 
 
 class VirusAgent(Agent):
-    """Individual Agent definition and its properties/interaction methods."""
+    """Defines individual agents and their interactions."""
 
-
-    def __init__(
-        self,
-        model,
-        initial_state,
-        virus_spread_chance,
-        virus_check_frequency,
-        # recovery_chance,
-        # gain_resistance_chance,
-        resistance_duration,
-    ):
+    def __init__(self, model, initial_state, virus_spread_chance, virus_check_frequency, resistance_duration, name=None):
         super().__init__(model)
         self.state = initial_state
         self.virus_spread_chance = virus_spread_chance
         self.virus_check_frequency = virus_check_frequency
         self.influence_chance = virus_check_frequency * 0.7
-        # self.recovery_chance = recovery_chance
-        # self.gain_resistance_chance = gain_resistance_chance
         self.resistance_duration = resistance_duration
         self.resistance_counter = 0
-
+        self.infecting_strain = None  # Track the misinformation strain
+        self.name = name  # Name for specific agents like "Misinformed User 1"
 
     def try_to_infect_neighbors(self):
-        neighbors_nodes = self.model.grid.get_neighborhood(
-            self.pos, include_center=False
-        )
+        """Infect susceptible neighbors with the misinformation strain of the infecting agent."""
+        neighbors_nodes = self.model.grid.get_neighborhood(self.pos, include_center=False)
         susceptible_neighbors = [
-            agent
-            for agent in self.model.grid.get_cell_list_contents(neighbors_nodes)
-            if agent.state is State.SUSCEPTIBLE
+            agent for agent in self.model.grid.get_cell_list_contents(neighbors_nodes) if agent.state == State.SUSCEPTIBLE
         ]
+
         for a in susceptible_neighbors:
             if self.random.random() < self.virus_spread_chance:
                 a.state = State.MISINFORMED_USER
+                
+                # Assign the infection strain based on the infector
                 if self.state in [State.MISINFORMATION_BOT, State.MISINFORMATION_BOT_2, State.MISINFORMATION_BOT_3]:
                     self.model.botInfected += 1
-                    # print(f"Bot infected: {botInfected}")
-                elif self.state is State.MISINFORMED_USER:
+                    a.infecting_strain = self.state  # Track which bot strain infected the user
+                elif self.state == State.MISINFORMED_USER and self.infecting_strain:
                     self.model.userInfected += 1
-                    # print(f"User infected: {userInfected}")
-
-
-    # def try_gain_resistance(self):
-    #     if self.random.random() < self.gain_resistance_chance:
-    #         self.state = State.RESISTANT
-
-
-    # def try_remove_infection(self):
-    #     if self.random.random() < self.recovery_chance:
-    #         self.state = State.SUSCEPTIBLE
-    #         self.try_gain_resistance()
-    #     else:
-    #         self.state = State.MISINFORMED_USER
-
+                    a.infecting_strain = self.infecting_strain  # Maintain strain lineage
 
     def try_fact_check(self):
-        neighbors_nodes = self.model.grid.get_neighborhood(
-            self.pos, include_center=False
-        )
+        """Fact-check nearby misinformed users and turn them resistant."""
+        neighbors_nodes = self.model.grid.get_neighborhood(self.pos, include_center=False)
         misinformed_neighbors = [
-            agent
-            for agent in self.model.grid.get_cell_list_contents(neighbors_nodes)
-            if agent.state is State.MISINFORMED_USER
+            agent for agent in self.model.grid.get_cell_list_contents(neighbors_nodes) if agent.state == State.MISINFORMED_USER
         ]
+
         for a in misinformed_neighbors:
             if self.random.random() < self.virus_check_frequency:
                 a.state = State.RESISTANT
                 a.resistance_counter = self.resistance_duration
 
-
     def try_influence_misinformed(self):
-        neighbors_nodes = self.model.grid.get_neighborhood(
-            self.pos, include_center=False
-        )
+        """Resistant agents attempt to influence misinformed neighbors."""
+        neighbors_nodes = self.model.grid.get_neighborhood(self.pos, include_center=False)
         misinformed_neighbors = [
-            agent
-            for agent in self.model.grid.get_cell_list_contents(neighbors_nodes)
-            if agent.state is State.MISINFORMED_USER
+            agent for agent in self.model.grid.get_cell_list_contents(neighbors_nodes) if agent.state == State.MISINFORMED_USER
         ]
+
         for a in misinformed_neighbors:
             if self.random.random() < self.influence_chance:
                 a.state = State.RESISTANT
                 a.resistance_counter = self.resistance_duration
 
     def step(self):
-        if self.state in [State.MISINFORMED_USER, State.MISINFORMATION_BOT]:
+        """Defines the behavior of each agent in a single simulation step."""
+        if self.state in [State.MISINFORMED_USER, State.MISINFORMATION_BOT, State.MISINFORMATION_BOT_2, State.MISINFORMATION_BOT_3]:
             self.try_to_infect_neighbors()
-        elif self.state is State.FACT_CHECKER:
+        elif self.state == State.FACT_CHECKER:
             self.try_fact_check()
-        elif self.state is State.RESISTANT:
+        elif self.state == State.RESISTANT:
             self.try_influence_misinformed()
             self.resistance_counter -= 1
             if self.resistance_counter <= 0:
-                self.state = State.SUSCEPTIBLE
-        # self.try_check_situation()
-
-
-    # def try_check_situation(self):
-    #     if (self.random.random() < self.virus_check_frequency) and (
-    #         self.state is State.MISINFORMED_USER
-    #     ):
-    #         self.try_remove_infection()
-
-
+                self.state = State.SUSCEPTIBLE  # Resistant users can become susceptible again
