be1cdef4cf702fca1ab765c930bd06741e18650c
Kyle Chandrasena <kchandrasena10@gmail.com>
2025-03-27 21:15:35 -0400

    Added resistance relapse functionality

--- src/InterimPrototype/virus_on_network/model.py
+++ src/InterimPrototype/virus_on_network/model.py
@@ -134,7 +134,6 @@ class VirusOnNetwork(Model):
         # gain_resistance_chance=0.5,
         resistance_duration=6,
         fact_checker_ratio=0.1,
-        misinformation_bot_ratio=0.1,
         seed=None,
     ):
         super().__init__(seed=seed)
